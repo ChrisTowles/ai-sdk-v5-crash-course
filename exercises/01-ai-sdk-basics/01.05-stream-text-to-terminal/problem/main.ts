70203242e9cbbc21858f6b8cbeccbc7408f1cb62
Chris Towles <Chris.Towles@gmail.com>
2025-10-24 23:06:32 -0400

    complete 1.05

--- exercises/01-ai-sdk-basics/01.05-stream-text-to-terminal/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.05-stream-text-to-terminal/problem/main.ts
@@ -1,12 +1,12 @@
-import { google } from '@ai-sdk/google';
+import { anthropic } from '@ai-sdk/anthropic';
 import { streamText } from 'ai';
 
-const model = google('gemini-2.0-flash');
+const model = anthropic('claude-3-haiku-20240307');
 
 const prompt =
   'Give me the first paragraph of a story about an imaginary planet.';
 
-const stream = TODO; // TODO - stream some text with the model above.
+const stream = await streamText({model, prompt}); // TODO - stream some text with the model above.
 
 for await (const chunk of stream.textStream) {
   process.stdout.write(chunk);
